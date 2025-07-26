f51879c6ea9d16bdca0409a39dc27ab83b4647ca
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-07-26 12:56:35 -0500

    adding usersSlice

--- practicing/src/redux/store.js
+++ practicing/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import usersSlice from './usersSlice'
 import productsReducer from './productsSlice'
 
 const store = configureStore({
 	reducer: {
+		users: usersSlice,
 		products: productsReducer
 	}
 })
