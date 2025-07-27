e04583dba3dfc3080e910ceac154727c46cf1822
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-07-26 23:18:05 -0500

    Testing with redux proxies

--- practicing/src/redux/productsSlice.js
+++ practicing/src/redux/productsSlice.js
@@ -3,13 +3,17 @@ import { createSlice } from '@reduxjs/toolkit'
 const productsSlice = createSlice({
 	name: 'products',
 	initialState: {
-		data: []
+		data: [{name: 'jj', id: 99}]
 	},
 	reducers: {
 		createProduct: (state, action) => {},
 		readProducts: (state, action) => {
 			// console.log(action.payload)
-			// console.log(state.data)
+			// console.log(state)
+			/*
+			const clone = JSON.parse(JSON.stringify(state));
+  		console.log(clone); // Aquí sí ves: { data: [...] }
+			*/
 			state.data = action.payload
 		},
 		updateProduc: (state, action) => {},
