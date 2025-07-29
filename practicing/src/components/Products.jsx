4b386c4e073ed3031366bac3d2acbec33d4a5f23
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-07-29 11:42:14 -0500

    update product in db.json

--- practicing/src/components/Products.jsx
+++ practicing/src/components/Products.jsx
@@ -1,27 +1,35 @@
-import { useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import axios from 'axios'
 import { useSelector, useDispatch } from 'react-redux'
 import { createProduct, readProducts } from '../redux/productsSlice'
 
 function Products() {
+	const [productToUpdate, setProductToUpdate] = useState({ id: null, name: null })
 	const newNameProduct = useRef('')
+
 	const handleCreateProduct = () => {
 		const valueNewNameProduct = newNameProduct.current.value
 		if (!valueNewNameProduct) return
-
 		const newProduct = {
 			id: Date.now(),
 			name: valueNewNameProduct
 		}
-
 		dispatch(createProduct(newProduct))
-
 		axios
 			.post('http://localhost:3001/products', newProduct)
 			.then(() => {
 				newNameProduct.current.value = ''
 				console.log('Nombre de producto guardado en JSON Server')
 			})
+			.catch(err => console.error(err))
+	}
+
+	const handleUpdateProduct = () => {
+		axios.
+			put(`http://localhost:3001/products/${productToUpdate.id}`, {
+				name: productToUpdate.name
+			})
+			.then(() => setProductToUpdate({ id: null, name: null }))
 			.catch(err => console.error(err))
 	}
 
@@ -43,7 +51,29 @@ function Products() {
 			<ul>
 				{products.data.map(product =>
 					<li key={product.id}>
-						{product.name}
+						{
+							productToUpdate.id === product.id
+							?
+							<div className="editProduct">
+								<input
+									type="text"
+									value={productToUpdate.name}
+									onChange={e => setProductToUpdate(
+										{ ...productToUpdate, name: e.target.value }
+									)}
+									autoFocus={true}
+								/>
+								<button onClick={handleUpdateProduct}>Guardar</button>
+							</div>
+							:
+							<div className="button">
+								<span>{product.name}</span>
+								<button onClick={() =>
+									setProductToUpdate({ id: product.id, name: product.name })
+								}>Editar</button>
+								<button>Eliminar</button>
+							</div>
+						}
 					</li>
 				)}
 				<input
