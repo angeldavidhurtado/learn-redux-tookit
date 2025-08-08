7efb9aecf8d514c643d6cdca064ccc49b906e144
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-08 14:54:22 -0500

    deploy

--- medium-test/vite.config.js
+++ medium-test/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/learn-redux-toolkit/medium',
+  base: '/learn-redux-toolkit/medium-test',
   plugins: [react()],
 })
--- practicing-to-deploy/vite.config.js
+++ practicing-to-deploy/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/learn-redux-toolkit/practicing',
+  base: '/learn-redux-toolkit/practicing-to-deploy',
   plugins: [react()],
 })
