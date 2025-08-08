84c98a0038ed4a3c7e53e086b5e766a65a5c52c5
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-08 14:47:12 -0500

    deploy

--- practicing-to-deploy/vite.config.js
+++ practicing-to-deploy/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/learn-redux-toolkit/',
+  base: '/learn-redux-toolkit/practicing',
   plugins: [react()],
 })
